81c46b9a6131c7dcc9d602994b9a123c03ed75e9
gopisunkara52 <gsunkara52@gmail.com>
2023-05-18 11:54:20 +0530

    ganarateOtpTwoScreens and EnterOtp Two Screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// import GanarateOtp from './screens/GanarateOtp'
+// import EnterOtp from './screens/EnteOtp'
+// function App() {
+ 
 
+//   return (
+//     //  <GanarateOtp/>
+//     <EnterOtp/>
+//   )
+// }
+
+// export default App
+
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ChakraProvider } from '@chakra-ui/react';
+import Genarateotp from './screens/Genarateotp';
+import Enterotp from './screens/Enterotp';
+import GanarateOtp from './screens/GanarateOtp';
+import EnterOtp from './screens/EnteOtp';
+
+
+
+
+
+
+
+
+
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <ChakraProvider>
+      <Routes>
+      {/* <Route path="/" element={<Genarateotp/>} />
+      <Route path="/enterOtp" element={<Enterotp/>} /> */}
+
+        {/* <Route path="/enterotp" element={<EnterOtp />} /> */}
+        
+        
+        <Route path="/" element={<GanarateOtp />} />
+        <Route path="/enterotp" element={<EnterOtp />} />
+        
+      </Routes>
+      </ChakraProvider>
+    </Router>
+  );
 }
 
-export default App
+export default App;
+
